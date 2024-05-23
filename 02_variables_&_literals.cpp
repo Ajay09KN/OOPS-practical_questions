70ba2df962275ff5a37030960b69692b8ef7acf8
Ajay Naroliya <166037578+Ajay09KN@users.noreply.github.com>
2024-05-23 12:21:13 +0530

    Update 02_variables_&_literals.cpp

--- 02_variables_&_literals.cpp
+++ 02_variables_&_literals.cpp
@@ -1,1 +1,11 @@
+/*Store two integers in two variables x and y. Print the product of the two.*/
 
+#include <iostream>
+using namespace std;
+int main()
+{  
+  int x =9;
+	int y =7;
+	cout << x*y << endl;
+	return 0;
+}
